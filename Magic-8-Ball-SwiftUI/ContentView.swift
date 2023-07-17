79e365bf1c2f65b558134bf5d15e39107068e8a2
Bob Witmer <bwitmer@golden.net>
2023-07-17 16:51:01 -0400

    Version 1.0

--- Magic-8-Ball-SwiftUI/ContentView.swift
+++ Magic-8-Ball-SwiftUI/ContentView.swift
@@ -8,12 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var imageName = Image("8-ball")
+    @State private var animateImage = false
+    @State private var playAgainHidden = true
+    @State private var messageAnswer = ""
+    @State private var previousAnswer = ""
+    @State private var answersArray = ["It is certain.",
+                                       "As I see it, yes.",
+                                       "Reply hazy, try again.",
+                                       "Don't count on it.",
+                                       "It is decidedly so.",
+                                       "Most likely.",
+                                       "Ask again later.",
+                                       "My reply is no.",
+                                       "Without a doubt.",
+                                       "Outlook good.",
+                                       "Better not tell you now.",
+                                       "My sources say no.",
+                                       "Yes definitely.",
+                                       "Yes.",
+                                       "Can not predict now.",
+                                       "Outlook not so good.",
+                                       "You may rely on it.",
+                                       "Signs point to yes.",
+                                       "Concentrate and ask again.",
+                                       "Very doubtful."]
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+                imageName
+                    .resizable()
+                    .scaledToFit()
+                    .animation(.easeIn(duration: 0.75), value: imageName)
+                
+                Text(messageAnswer)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(.blue)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(.blue, lineWidth: 2)
+                    }
+            
+            Spacer()
+            
+            Button("Ask a Question") {
+                repeat {
+                    if !answersArray.isEmpty {
+                        messageAnswer = answersArray.randomElement()!
+                        imageName = Image("8-ball-blue")
+                        animateImage = true
+                        playAgainHidden = false
+                    } else {
+                        messageAnswer = "😡 Error: No answers in my array."
+                    }
+                } while messageAnswer == previousAnswer
+                previousAnswer = messageAnswer
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.largeTitle)
+ 
+            Spacer()
+            
+            if !playAgainHidden {
+                Button("Reset") {
+                    imageName = Image("8-ball")
+                    messageAnswer = ""
+                    playAgainHidden = true
+                }
+                .font(.largeTitle)
+                .foregroundColor(.green)
+            }
+            
         }
         .padding()
     }
