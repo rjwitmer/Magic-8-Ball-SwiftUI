0c566e887b40b4b15cb5f17736dbab0203d87569
Bob Witmer <bwitmer@golden.net>
2023-07-17 11:24:30 -0400

    Initial Commit

---
+++ Magic-8-Ball-SwiftUI/Magic_8_Ball_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_8_Ball_SwiftUIApp.swift
+//  Magic-8-Ball-SwiftUI
+//
+//  Created by Bob Witmer on 2023-07-17.
+//
+
+import SwiftUI
+
+@main
+struct Magic_8_Ball_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
